2773a7214e282f6f673483f5233b880505947c3f
Egbert Eich <eich@suse.de>
2004-04-23 18:42:32 +0000

    Merging XORG-CURRENT into trunk

--- src/XpmI.h
+++ src/XpmI.h
@@ -22,7 +22,7 @@
  * used in advertising or otherwise to promote the sale, use or other dealings
  * in this Software without prior written authorization from GROUPE BULL.
  */
-/* $XFree86: xc/extras/Xpm/lib/XpmI.h,v 1.9 2003/08/06 14:03:58 eich Exp $ */
+/* $XFree86: xc/extras/Xpm/lib/XpmI.h,v 1.7 2001/11/01 23:35:25 dawes Exp $ */
 
 /*****************************************************************************\
 * XpmI.h:                                                                     *
